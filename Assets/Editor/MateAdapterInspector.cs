57f4a0007da4f4ab0eeb1359462444fcb91e0f84
Martin Tribo <martin.tribo@gmail.com>
2016-02-12 19:23:34 -0600

    Added Zero Gravity Player, another default scene station and changed input for controller

--- Assets/Editor/MateAdapterInspector.cs
+++ Assets/Editor/MateAdapterInspector.cs
@@ -13,7 +13,7 @@ public class MateAdapterInspector : Editor {
 
 		if (GUILayout.Button("Attach New Module")) {
 			if (thisAdapter.connectedAdapter == null) {
-				GameObject module = Instantiate(Resources.Load("Prefabs/Samples/StraightModule") as GameObject);
+				GameObject module = PrefabUtility.InstantiatePrefab(Resources.Load("Prefabs/Samples/StraightModule") as GameObject) as GameObject;
 				module.GetComponent<Module>().mateAdapters[0].ConnectToAdapter(thisAdapter);
 			} else {
 				Debug.Log("Adapter is already connected to another adapter!");
@@ -22,7 +22,7 @@ public class MateAdapterInspector : Editor {
 
 		if (GUILayout.Button("Attach New Connector")) {
 			if (thisAdapter.connectedAdapter == null) {
-				GameObject module = Instantiate(Resources.Load("Prefabs/Samples/ConnectorModule") as GameObject);
+				GameObject module = PrefabUtility.InstantiatePrefab(Resources.Load("Prefabs/Samples/ConnectorModule") as GameObject) as GameObject;
 				module.GetComponent<Module>().mateAdapters[0].ConnectToAdapter(thisAdapter);
 			} else {
 				Debug.Log("Adapter is already connected to another adapter!");
---
+++ Assets/Scripts/ZeroGravityPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZeroGravityPlayer : MonoBehaviour {
+	public float speed = 5.0F;
+	public float rotateSpeed = 2.0F;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var straight = Input.GetAxisRaw("Vertical");
+		var strafe = Input.GetAxisRaw("Horizontal");
+		var pitch = Input.GetAxisRaw("Pitch");
+		var yaw = Input.GetAxisRaw("Yaw");
+		var rigidBody = gameObject.GetComponent<Rigidbody>();
+		var up = Input.GetAxisRaw("Up");
+		var down = Input.GetAxisRaw("Down");
+		var roll = Input.GetAxisRaw("Roll");
+
+		rigidBody.AddRelativeForce(speed * new Vector3(strafe, up - down, straight) * speed);
+		rigidBody.AddRelativeTorque(new Vector3(pitch, yaw, roll) * rotateSpeed);
+	}
+}
